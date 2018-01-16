0a10a8667067a6965bddeabf4b82c35521f7a303
velizargenov <velizar.genov@gmail.com>
2018-01-16 14:14:25 +0000

    Add styling to the ResultTable component * Style the top part of the component

--- src/components/ResultTable/ResultTable.js
+++ src/components/ResultTable/ResultTable.js
@@ -3,11 +3,15 @@ import Flight from '../Flight/Flight';
 import './ResultTable.css';
 
 const ResultTable = ({ flights }) => (
-  <div className="result-table--wrapper">
+  <div className="result-table">
     <h2>Your results</h2>
-    <section>
-      <span>Depart: 1st Jan 2014;</span>
-      <span>Return: 10th Jan 2014;</span>
+    <section >
+      <span className="result-table--summary depart">
+        <span className="label">Depart:</span> 1st Jan 2014
+      </span>
+      <span className="result-table--summary return">
+        <span className="label">Return:</span> 10th Jan 2014
+      </span>
     </section>
     {
       flights.map(flight => <Flight key={flight.id} details={flight} />)
