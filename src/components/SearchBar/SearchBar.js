9d0f6e53abaaae3ee29a9f6810d461e70238f97a
velizargenov <velizar.genov@gmail.com>
2018-01-17 10:41:58 +0000

    Add price range slider * Overwrite default styles from 'react-input-range/lib/css/index.css'; * Cleanup SearchBar component * Create a TODO list for SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,7 +8,14 @@ import 'react-input-range/lib/css/index.css';
 import './SearchBar.css';
 import Button from '../Button/Button';
 
-// TODO: Extract InputField into its own folder; could combine with <Button />
+/**
+ * TODO: Extract <InputField /> into its own folder; could combine with <Button />
+ * TODO: Extract <InputDate /> into its own folder
+ * TODO: Create <InputRange /> component; extract it in its own folder
+ * TODO: Create a separate stylesheet for <InputRange />
+ * TODO: Create <Form /> component; extract it in its own folder
+ */
+
 const InputField = ({ htmlFor, text, ...otherProps }) => (
   <label htmlFor={htmlFor} className="input">
     <span className="input--text">{text}</span>
@@ -29,42 +36,6 @@ const InputDate = ({ dateType, text, handleChange }) => (
     />
   </div>
 );
-
-// const SearchBar = () => (
-//   <div className="search-bar">
-//     <div className="search-bar--buttons">
-//       <Button htmlFor="one-way" className="button--search-bar one-way" value="One way" />
-//       <Button htmlFor="return" className="button--search-bar return" value="Return" />
-//     </div>
-//     <form>
-//       <InputField
-//         htmlFor="fromInput"
-//         text="From"
-//         placeholder="Enter your input here..."
-//         type="text"
-//       />
-//       <InputField
-//         htmlFor="fromInput"
-//         text="Destination"
-//         placeholder="Enter your input here..."
-//         type="text"
-//       />
-
-//       <InputDate dateType="departure" text="Departure Date" />
-//       <InputDate dateType="Return Date" text="Return Date" />
-//       <InputDate dateType="Passengers" text="Passengers" />
-
-//       <Button htmlFor="search" className="search-bar--button-search" value="Search" />
-//     </form>
-
-//     <InputRange
-//       maxValue={20}
-//       minValue={0}
-//       value={10}
-//       onChange={value => this.setState({ value })}
-//     />
-//   </div>
-// );
 
 class SearchBar extends Component {
   constructor (props) {
@@ -97,18 +68,26 @@ class SearchBar extends Component {
 
           <InputDate dateType="departure" text="Departure Date" />
           <InputDate dateType="Return Date" text="Return Date" />
-          <InputDate dateType="Passengers" text="Passengers" />
+          <InputField
+            htmlFor="passenger"
+            text="Passenger"
+            placeholder="Enter passengers number..."
+            type="number"
+          />
 
           <Button htmlFor="search" className="search-bar--button-search" value="Search" />
         </form>
 
-        <InputRange
-          step={10}
-          minValue={0}
-          maxValue={200}
-          value={this.state.rangeValue}
-          onChange={rangeValue => this.setState({ rangeValue })}
-        />
+        <div className="search-bar--input-range">
+          <span className="input input--text">Refine Flight Search</span>
+          <InputRange
+            step={10}
+            minValue={0}
+            maxValue={200}
+            value={this.state.rangeValue}
+            onChange={rangeValue => this.setState({ rangeValue })}
+          />
+        </div>
       </div>
     );
   }
